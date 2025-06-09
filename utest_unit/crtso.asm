de126f97f4d1bce84b84199199990bc8562fc2d9
archeocomp <archeocomp@mail.com>
2025-06-09 15:07:01 +0200

    crtso cleaned

--- utest_unit/crtso.asm
+++ utest_unit/crtso.asm
@@ -6,12 +6,6 @@
 
         .globl  main
 
-TRAP_ROUTINNE   .equ    0xFFF2  ; interrupt vector - 3 bytes (C3 XXXX)
-RST55_ROUTINNE  .equ    0xFFEF  ; interrupt vector - 3 bytes (C3 XXXX)
-RST65_ROUTINNE  .equ    0xFFEC  ; interrupt vector - 3 bytes (C3 XXXX)
-RST75_ROUTINNE  .equ    0xFFE9  ; interrupt vector - 3 bytes (C3 XXXX)
-STATUS_CMD_REG_8155 .equ 0x00   ; i/o address of 8155
-
 init:
         di                      ; disable all interrupts
         lxi     sp,0xFFE0       ; set stack pointer
