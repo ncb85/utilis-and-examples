1723f1e41710a550f19ea978eb2fc22affa25c06
archeocomp <roman.fulek@gmail.com>
2025-07-06 08:58:27 +0200

    path simplified

--- cpm_fdc/settings.mk
+++ cpm_fdc/settings.mk
@@ -4,5 +4,5 @@
 SYSTEM_PATH := $(PATH)
 PROJECT_BINS := /home/pi/Programs/SmallC-85/smallC:/home/pi/Programs/asxv5p50/asxmak/linux/build
 
-export PATH := $(foreach dir,$(PROJECT_BINS),$(dir):)$(SYSTEM_PATH)
+export PATH := $(SYSTEM_PATH):$(PROJECT_BINS)
 
