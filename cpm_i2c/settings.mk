c8e4f8d9eacfc5ecf42b450e60c221b4e3e0049e
NCB85 <roman.fulek@gmail.com>
2025-07-07 21:20:23 +0200

    PATH in includes settings

---
+++ cpm_i2c/settings.mk
@@ -0,0 +1,9 @@
+#
+# file paths to tools used in MAKE, change as needed
+#
+SYSTEM_PATH := $(PATH)
+PROJECT_BINS := /home/pi/Programs/SmallC-85/smallC:/home/pi/Programs/asxv5p50/asxmak/linux/build
+
+export PATH := $(SYSTEM_PATH):$(PROJECT_BINS)
+
+export INSTALL_PATH := /home/pi/Programs/S-ilent-Drive/in
